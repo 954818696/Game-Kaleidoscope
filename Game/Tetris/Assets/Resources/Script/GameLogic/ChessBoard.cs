6ec78225d424113d84ee52cf0ef3af5a9a54b8d8
954818696 <954818696@qq.com>
2015-12-21 18:24:12 +0800

    basic func

---
+++ Game/Tetris/Assets/Resources/Script/GameLogic/ChessBoard.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GameLogic
+{
+    public enum EChessBoardSlot
+    {
+        E_OutOfRange = 0,
+        E_Empty      = 1,
+        E_Full       = 2,
+    }
+
+    public class ChessBoard
+    {
+        private bool[,] slotStateArray;
+        
+        public ChessBoard()
+        {
+            int width  = GameConfig.Instance.Width;
+            int height = GameConfig.Instance.Height;
+            slotStateArray = new bool[height, width];
+            
+            for (int y = 0; y < height; ++y)
+            {
+                for (int x = 0; x < width; ++x)
+                {
+                    slotStateArray[y, x] = false;
+                }
+            }
+        }
+        
+        public Vector3 GetSlotPos(int x, int y)
+        {
+            Vector3 pos = new Vector3(GameConfig.Instance.mLeftBottomPos.localPosition.x + x * GameConfig.Instance.blockWidth, 
+                                      GameConfig.Instance.mLeftBottomPos.localPosition.y + y * GameConfig.Instance.blockHeight, 
+                                      0);
+            
+            return pos;
+        }
+        
+        public EChessBoardSlot GetSlotState(int x, int y)
+        {
+            if (x < 0 || x >= GameConfig.Instance.Width ||
+                y < 0 || y >= GameConfig.Instance.Height)
+            {
+                return EChessBoardSlot.E_OutOfRange;
+            }
+
+            if (slotStateArray [y, x])
+            {
+                return EChessBoardSlot.E_Full;
+            }
+            else
+            {
+                return EChessBoardSlot.E_Empty;
+            }
+        }
+    }
+}
+
+
---
+++ Game/Tetris/Assets/Resources/Script/test.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+    public int BoardWidth = 10;
+    public int BoardHeight = 20;
+
+    public Transform mBlock;
+    public Transform mLeftBottomPos;
+    public float blockWidth;
+    public float blockHeight;
+
+	// Use this for initialization
+	void Start () {
+        testSetPos();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void testSetPos()
+    {
+        for (int y = 0; y < 20; ++y)
+        {
+            for (int x = 0; x < 10; ++x)
+            {
+                Instantiate(mBlock, new Vector3(mLeftBottomPos.localPosition.x + x * blockWidth, mLeftBottomPos.localPosition.y + y * blockHeight, 0), Quaternion.identity);
+            }
+        }
+    }
+}
